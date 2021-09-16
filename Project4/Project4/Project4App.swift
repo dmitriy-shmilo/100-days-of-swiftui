880089635fed9675eea18311832a0af989008f7d
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-09-16 11:23:09 +0300

    Project 4.

---
+++ Project4/Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by Dmitriy Shmilo on 15.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
